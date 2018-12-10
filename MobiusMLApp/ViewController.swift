9351fe6f35aed12e7ba91b312bbcf0a37d683f95
David Krajewski <davidkrajewski@yahoo.com>
2018-12-09 20:09:34 -0500

    Working example of facial gesture recognition

--- MobiusMLApp/ViewController.swift
+++ MobiusMLApp/ViewController.swift
@@ -1,20 +1,155 @@
-//
-//  ViewController.swift
-//  MobiusMLApp
-//
-//  Created by David Krajewski on 12/9/18.
-//  Copyright © 2018 David Krajewski. All rights reserved.
-//
-
 import UIKit
+import FaceTrigger
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var previewContainer: UIView!
+    @IBOutlet var logTextView: UITextView!
+    
+    var faceTrigger: FaceTrigger?
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        logTextView.text = nil
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(pause), name: UIApplication.willResignActiveNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(unpause), name: UIApplication.didBecomeActiveNotification, object: nil)
     }
-
-
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        faceTrigger = FaceTrigger(hostView: previewContainer, delegate: self)
+        faceTrigger?.start()
+    }
+    
+    @objc private func pause() {
+        faceTrigger?.pause()
+    }
+    
+    @objc private func unpause() {
+        faceTrigger?.unpause()
+    }
+    
+    private func updateLog(_ eventText: String) {
+        
+        DispatchQueue.main.async {
+            let currentText = self.logTextView.text ?? ""
+            self.logTextView.text = "\(eventText)\n\n\(currentText)"
+        }
+    }
+    
+    @IBAction func clearLogAction() {
+        logTextView.text = nil
+    }
 }
 
+extension ViewController: FaceTriggerDelegate {
+    
+    // smile
+    func onSmileDidChange(smiling: Bool) {
+        updateLog("onSmileDidChange \(smiling)")
+    }
+    
+    func onSmile() {
+        updateLog("smile")
+    }
+    
+    func onBlinkLeftDidChange(blinkingLeft: Bool) {
+        updateLog("onBlinkLeftDidChange \(blinkingLeft)")
+    }
+    
+    // blink
+    func onBlinkLeft() {
+        updateLog("blink left")
+    }
+    
+    func onBlinkRightDidChange(blinkingRight: Bool) {
+        updateLog("onBlinkRightDidChange \(blinkingRight)")
+    }
+    
+    func onBlinkRight() {
+        updateLog("blink right")
+    }
+    
+    func onBlinkDidChange(blinking: Bool) {
+        updateLog("onBlinkDidChange \(blinking)")
+    }
+    
+    func onBlink() {
+        updateLog("blink")
+    }
+    
+    // brow down
+    func onBrowDownDidChange(browDown: Bool) {
+        updateLog("onBrowDownDidChange \(browDown)")
+    }
+    
+    func onBrowDown() {
+        updateLog("brow down")
+    }
+    
+    // brow up
+    func onBrowUpDidChange(browUp: Bool) {
+        updateLog("onBrowUpDidChange \(browUp)")
+    }
+    
+    func onBrowUp() {
+        updateLog("brow up")
+    }
+    
+    // cheek puff
+    func onCheekPuffDidChange(cheekPuffing: Bool) {
+        updateLog("onCheekPuffDidChange \(cheekPuffing)")
+    }
+    
+    func onCheekPuff() {
+        updateLog("cheek puff")
+    }
+    
+    // mouth pucker
+    func onMouthPuckerDidChange(mouthPuckering: Bool) {
+        updateLog("onMouthPuckerDidChange \(mouthPuckering)")
+    }
+    
+    func onMouthPucker() {
+        updateLog("mouth pucker")
+    }
+    
+    // jaw open
+    func onJawOpenDidChange(jawOpening: Bool) {
+        updateLog("onJawOpenDidChange \(jawOpening)")
+    }
+    
+    func onJawOpen() {
+        updateLog("jaw open")
+    }
+    
+    // jaw left
+    func onJawLeftDidChange(jawLefting: Bool) {
+        updateLog("onJawLeftDidChange \(jawLefting)")
+    }
+    
+    func onJawLeft() {
+        updateLog("jaw left")
+    }
+    
+    // jaw right
+    func onJawRightDidChange(jawRighting: Bool) {
+        updateLog("onJawRightDidChange \(jawRighting)")
+    }
+    
+    func onJawRight() {
+        updateLog("jaw right")
+    }
+    
+    // squint
+    func onSquintDidChange(squinting: Bool) {
+        updateLog("onSquintDidChange \(squinting)")
+    }
+    
+    func onSquint() {
+        updateLog("squint")
+    }
+}
